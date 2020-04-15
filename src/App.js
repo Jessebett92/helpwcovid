1f541dadecad0087e82762e155ce7ca4caeecf4e
jessebettridge <jessebett92@gmail.com>
2020-04-15 00:27:58 -0600

    crlf config w/ app progression

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router } from "@reach/router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Components/pages/home";
+import ConfTesting from "./Components/questions/conftesting";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loggedIn: "NOT_LOGGED_IN",
+    };
+  }
+
+  render() {
+    return (
+      <div className="pageWrapper">
+        <Router>
+          <Home path="/" />
+          <ConfTesting path="/questionOne" />
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Components/questions/conftesting.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ConfTesting = () => {
+  return (
+    <div>
+      <h1>Question One</h1>
+    </div>
+  );
+};
+
+export default ConfTesting;
